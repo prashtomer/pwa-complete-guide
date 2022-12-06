3ad035e866239dd97d6acfb52628b69c59c03657
prashtw <prasht@thoughtworks.com>
2022-12-06 22:07:09 +0530

    fetch data from firebase server and store it in the indexed db

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,7 @@
+importScripts('/src/js/idb.js')
+
 // Bump these versions if you make changes in any of the static files that you are caching below in install event
-var CACHE_STATIC_NAME = 'static-v15';
+var CACHE_STATIC_NAME = 'static-v16';
 var CACHE_DYNAMIC_NAME = 'dynamic-v2';
 var STATIC_FILES = [
   '/',
@@ -7,6 +9,7 @@ var STATIC_FILES = [
   '/offline.html',
   '/src/js/app.js',
   '/src/js/feed.js',
+  '/src/js/idb.js',
   '/src/js/promise.js',
   '/src/js/fetch.js',
   '/src/js/material.min.js',
@@ -17,6 +20,12 @@ var STATIC_FILES = [
   'https://fonts.googleapis.com/icon?family=Material+Icons',
   'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
 ]
+
+var dbPromise = idb.open('posts-store', 1, function (db) {
+  if(!db.objectStoreNames.contains('posts')) {
+    db.createObjectStore('posts', {keyPath: 'id'});
+  }
+})
 
 // function trimCache(cacheName, maxItems) {
 //   caches.open(cacheName)
@@ -110,14 +119,22 @@ self.addEventListener('fetch', function (event) { // cache then network
   var url = 'https://tomer-pwagram-default-rtdb.firebaseio.com/posts';
   if (event.request.url.indexOf(url) > -1) {
     event.respondWith(
-      caches.open(CACHE_DYNAMIC_NAME)
-        .then(function (cache) {
-          return fetch(event.request)
-            .then(function (res) {
-              // trimCache(CACHE_DYNAMIC_NAME, 3);
-              cache.put(event.request, res.clone());
-              return res;
+      fetch(event.request)
+        .then(function (res) {
+          var clonedRes = res.clone();
+          clonedRes.json()
+            .then(function (data) {
+              for(var key in data) {
+                dbPromise
+                  .then(function (db) {
+                    var tx = db.transaction('posts', 'readwrite');
+                    var store = tx.objectStore('posts');
+                    store.put(data[key]);
+                    return tx.complete;
+                  })
+              }
             })
+          return res;
         })
     );
   } else if (isInArray(event.request.url, STATIC_FILES)) { // cache only
