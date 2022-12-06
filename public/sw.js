42e60967ba3005c9606b6400e3fdde00bb4984e6
prashtw <prasht@thoughtworks.com>
2022-12-06 17:56:54 +0530

    comment trim cache function

--- public/sw.js
+++ public/sw.js
@@ -18,20 +18,20 @@ var STATIC_FILES = [
   'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
 ]
 
-function trimCache(cacheName, maxItems) {
-  caches.open(cacheName)
-    .then(function (cache) {
-      return cache.keys()
-        .then(function (keys) {
-          if (keys.length > maxItems) {
-            cache.delete(keys[0])
-              .then(function () {
-                trimCache(cacheName, maxItems)
-              });
-          }
-        });
-    })
-}
+// function trimCache(cacheName, maxItems) {
+//   caches.open(cacheName)
+//     .then(function (cache) {
+//       return cache.keys()
+//         .then(function (keys) {
+//           if (keys.length > maxItems) {
+//             cache.delete(keys[0])
+//               .then(function () {
+//                 trimCache(cacheName, maxItems)
+//               });
+//           }
+//         });
+//     })
+// }
 
 self.addEventListener('install', function (event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
@@ -114,7 +114,7 @@ self.addEventListener('fetch', function (event) { // cache then network
         .then(function (cache) {
           return fetch(event.request)
             .then(function (res) {
-              trimCache(CACHE_DYNAMIC_NAME, 3);
+              // trimCache(CACHE_DYNAMIC_NAME, 3);
               cache.put(event.request, res.clone());
               return res;
             })
@@ -136,7 +136,7 @@ self.addEventListener('fetch', function (event) { // cache then network
                 return caches.open(CACHE_DYNAMIC_NAME) // caching dynamic data
                   .then(function (cache) {
                     // put doesn't make request like add. It just stores the data you have.
-                    trimCache(CACHE_DYNAMIC_NAME, 3);
+                    // trimCache(CACHE_DYNAMIC_NAME, 3);
                     cache.put(event.request.url, res.clone());
                     return res;
                   })
