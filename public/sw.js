19f295d82dc5c24c761c40b5490297087070efa8
prashtw <prasht@thoughtworks.com>
2022-12-06 17:23:11 +0530

    replace regex matching with custom code

--- public/sw.js
+++ public/sw.js
@@ -78,6 +78,17 @@ self.addEventListener('activate', function (event) {
 //   );
 // });
 
+function isInArray(string, array) {
+  var cachePath;
+  if (string.indexOf(self.origin) === 0) { // request targets domain where we serve the page from (i.e. NOT a CDN)
+    console.log('matched ', string);
+    cachePath = string.substring(self.origin.length); // take the part of the URL AFTER the domain (e.g. after localhost:8080)
+  } else {
+    cachePath = string; // store the full request (for CDNs)
+  }
+  return array.indexOf(cachePath) > -1;
+}
+
 
 // Cache then Network Strategy for url and else go back to old strategy ie cache with network fallback
 self.addEventListener('fetch', function (event) { // cache then network
@@ -93,7 +104,7 @@ self.addEventListener('fetch', function (event) { // cache then network
             })
         })
     );
-  } else if(new RegExp('\\b' + STATIC_FILES.join('\\b|\\b') + '\\b').test(event.request.url)) { // cache only
+  } else if(isInArray(event.request.url, STATIC_FILES)) { // cache only
     event.respondWith(
       caches.match(event.request)
     );
