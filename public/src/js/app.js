1a3c01332f62f423cfc5867f8170c2207b145a50
prashtw <prasht@thoughtworks.com>
2022-12-06 14:47:23 +0530

    implement static and dynamic files caching using service worker

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,12 +1,22 @@
 // Registering in app.js because it's been added to all the html pages
 var deferredPrompt;
 
-if('serviceWorker' in navigator) {
+// check if Promise is supported or not
+// if not assign it from the polyfill added in html files
+if (!window.Promise) {
+  window.Promise = Promise;
+}
+
+if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('/sw.js') // scope here can be passed as the second paramter and that will override the scope created by placing the service worker file in the public folder.
     .then(function () {
       console.log('Service worker registered!');
+    })
+    .catch(function (err) {
+      console.log(err);
     });
+  ;
 }
 
 window.addEventListener('beforeinstallprompt', function (event) {
--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,72 @@
+// Bump these versions if you make changes in any of the files that you are caching
+var CACHE_STATIC_NAME = 'static-v4';
+var CACHE_DYNAMIC_NAME = 'dynamic-v2';
+
 self.addEventListener('install', function (event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+    caches.open(CACHE_STATIC_NAME) // open existing cache or create new
+      .then(function (cache) {
+        console.log('[Service Worker] Precaching App Shell');
+        // fetch from server and then put it in the cache to server the subsequent requests
+        // key in cache is the request object and not a string
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/src/js/app.js',
+          '/src/js/feed.js',
+          '/src/js/promise.js',
+          '/src/js/fetch.js',
+          '/src/js/material.min.js',
+          '/src/css/app.css',
+          '/src/css/feed.css',
+          '/src/images/main-image.jpg',
+          'https://fonts.googleapis.com/css?family=Roboto:400,700',
+          'https://fonts.googleapis.com/icon?family=Material+Icons',
+          'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+        ]);
+      })
+  );
 });
 
 self.addEventListener('activate', function (event) {
   console.log('[Service Worker] Activating Service Worker ...', event);
-  return self.clients.claim(); // it ensure the service workers are loaded correctly, can also work without this line but may behave strangely
+  event.waitUntil(
+    caches
+      .keys()
+      .then(function (keyList) {
+        return Promise.all(keyList.map(function (key) {
+          if(key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            console.log('[Service Worker] Removing old cache.', key);
+            return caches.delete(key);
+          }
+        }));
+      })
+  );
+  return self.clients.claim(); // it ensures that the service workers are loaded correctly, can also work without this line but may behave strangely
 });
 
 self.addEventListener('fetch', function (event) {
-  console.log('[Service Worker] Fetching something ...', event);
-  event.respondWith(fetch(event.request)); // let the request go as is.
+  // event.respondWith(fetch(event.request)); // let the request go as is.
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if(response) {
+          return response;
+        } else {
+          return  fetch(event.request)
+            .then(function (res) {
+              return caches.open(CACHE_DYNAMIC_NAME) // caching dynamic data
+                .then(function (cache) {
+                  // put doesn't make request like add. It just stores the data you have.
+                  cache.put(event.request.url, res.clone());
+                  return res;
+                })
+                .catch(function (err) {
+
+                })
+            });
+        }
+      })
+  );
 });
