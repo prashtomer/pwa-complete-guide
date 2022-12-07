ba702245e740ebbd47ade84cef551f3ddb5f6830
prashtw <prasht@thoughtworks.com>
2022-12-07 15:16:15 +0530

    push the subscription to the firebase database

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -54,9 +54,61 @@ function displayConfirmNotification() {
     };
     navigator.serviceWorker.ready
       .then(function (swreg) {
-        swreg.showNotification('Successfully subscribed (from SW)!', options);
+        swreg.showNotification('Successfully subscribed!', options);
       });
   }
+}
+
+function configurePushSub() {
+  if (!('serviceWorker' in navigator)) {
+    return;
+  }
+
+  var reg;
+
+  navigator.serviceWorker.ready
+    .then(function (swreg) {
+      reg = swreg;
+      return swreg.pushManager.getSubscription();
+    })
+    .then(function (sub) {
+      if (sub === null) {
+        // create a new subscription
+        // for this we need credit card info in firebase and then we have to initialise this project with firebase tooling
+        // This will generated a folder called functions in the root where we can install this webpush package used for generating the public and private key
+        // This cloud functions featuer in firebase requires credit card information
+        // without it we won't have server for sending push messages to trigger push notifications
+
+        var vapidPublicKey = 'some-generated-key-through-webpush-package';
+        var convertedVapidPublicKey = urlBase64ToUint8Array(vapidPublicKey);
+        return reg.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: convertedVapidPublicKey,
+        });
+      } else {
+        // we have subscription
+      }
+    })
+    .then(function (newSub) {
+      // here we are pushing this new subscription to the firbase server
+      // And is the firebase database under subscriptions you will see this subscription with endpoint which is a vendor endpoint like google where we can then send push messages to.
+      return fetch('https://tomer-pwagram-default-rtdb.firebaseio.com/subscriptions.json', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: JSON.stringify(newSub)
+      });
+    })
+    .then(function (res) {
+      if(res.ok) {
+        displayConfirmNotification();
+      }
+    })
+    .catch(function (err) {
+      console.log(err);
+    })
 }
 
 function askForNotificationPermission() {
@@ -65,12 +117,13 @@ function askForNotificationPermission() {
     if (result !== 'granted') {
       console.log('No notification permission granted!');
     } else {
-      displayConfirmNotification();
+      configurePushSub();
+      // displayConfirmNotification();
     }
   });
 }
 
-if ('Notification' in window) {
+if ('Notification' in window && 'serviceWorker' in navigator) {
   for (var i = 0; i < enableNotificationsButtons.length; i++) {
     enableNotificationsButtons[i].style.display = 'inline-block';
     enableNotificationsButtons[i].addEventListener('click', askForNotificationPermission);
